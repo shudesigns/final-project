7ebb34873790232cc771c794e24bc698c171faf9
shuweitsai <shuweitsai@chtsecurity.com>
2022-12-28 17:34:28 +0800

    feat: modify areas

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -45,8 +45,17 @@
                 </div>
                 <template v-for="area in areas" :key="area.key">
                     <Transition name="fade">
-                        <div class="map-area" v-if="area.image" v-show="area.key === active_area">
-                            <img :src="require(`@/assets/images/${area.image}`)" class="d-block w-100" :alt="area.name" />
+                        <div
+                            :class="`map-area area-${area.key} ${area.key === active_area ? 'active' : ''}`"
+                            v-if="area.image"
+                            v-show="area.key === active_area">
+                            <div class="area-image">
+                                <div class="toggle" @mouseenter="area.show_desc = true" @mouseleave="area.show_desc = false"></div>
+                                <img :src="require(`@/assets/images/${area.image}`)" class="d-block w-100" :alt="area.name" />
+                            </div>
+                            <Transition name="fade">
+                                <div class="area-description" v-if="area.show_desc" v-html="area.description"></div>
+                            </Transition>
                         </div>
                     </Transition>
                 </template>
@@ -89,27 +98,35 @@ export default {
             areas: [
                 {
                     key: 'all',
-                    name: '顯示全部'
+                    name: '首頁'
                 },
                 {
                     key: 1,
                     name: '宮廷區',
-                    image: 'map-area-01.png'
+                    image: 'map-area-01.png',
+                    description: `<h5>宮殿區：移動的紫禁城</h5><p class="mb-0">避暑山莊宮殿區位於山莊南部，是清朝皇帝駐蹕山莊時處理朝政、舉行慶典與寢居空間。由正宮、東宮、萬壑松風與松鶴齋四部分組成。正宮建築規格依循北京紫禁城中軸線前朝後寢布局興建，具有對稱、規整與莊嚴特點。前朝以澹泊敬誠殿，後寢則以煙波致爽殿為代表。清代中晚期，嘉慶與咸豐兩位皇帝先後在山莊駕崩，國勢也開始慢慢進入夕陽黃昏。</p>`,
+                    show_desc: false
                 },
                 {
                     key: 2,
                     name: '湖泊區',
-                    image: 'map-area-02.png'
+                    image: 'map-area-02.png',
+                    description: `<h5>湖泊區：文化與物資的匯聚</h5><p class="mb-0">山莊湖泊區緊鄰於宮殿區，由九個大小湖泊組成，湖內島嶼林立，各有名稱，如文園島、如意洲、青蓮島、金山、月色江聲島等。島湖小橋長堤相連，樓閣亭榭，綠樹成蔭，不僅部分植物盆栽移植自帝國內地，區內建築也有模仿自江南園林布局，例如青蓮島的煙雨樓是據浙江嘉興煙雨樓而建，文園獅子林則仿自蘇州獅子林，金山島設計是來自鎮江金山，一派江南水鄉風光，因此有「塞外江南」之稱。湖泊區是清代帝后駐蹕山莊時避暑休憩去處，康熙、乾隆兩帝更為山莊各取三十六美景，留下許多官方刊物與圖繪。</p>`,
+                    show_desc: false
                 },
                 {
                     key: 3,
                     name: '平原區',
-                    image: 'map-area-03.png'
+                    image: 'map-area-03.png',
+                    description: `<h5>平原區：外交場域</h5><p class="mb-0">平原區位於避暑山莊東北部，區內分東西兩部，西部草地以「試馬埭」為主體，是清帝與蒙古王公圍獵前試馬選馬的場地；東部林地則以「萬樹園」為代表，園區佈滿大大小小蒙古包，呈現北方蒙古草原景色。康熙年間萬樹園作為山莊行獵之處，而到了乾隆時期進行大規模擴建，賦予了此區在聯結外藩蒙藏王公貴族、宗教領袖以及國外使節外交朝覲筵宴的重要功能。</p>`,
+                    show_desc: false
                 },
                 {
                     key: 4,
-                    name: '山岳寺廟區',
-                    image: 'map-area-04.png'
+                    name: '寺廟山嶽區',
+                    image: 'map-area-04.png',
+                    description: `<h5>寺廟山嶽區：信仰與政治</h5><p class="mb-0">環繞山莊周邊外圍，自北至東南，沿獅子溝至武烈河東岸丘陵山巒區，漢藏式的寺廟林立，其中以北京理藩院統一管理的古北口外八座寺廟，俗稱的「外八廟」最為著名，是清帝國為紀念重要大事時特地興建的寺廟。例如為康熙五十二年(1713)紀念蒙古王公首次前來山莊覲見並為康熙皇帝祝壽而建的溥仁寺與溥善寺；乾隆二十年代初期為慶祝平定準噶爾蒙古而建的普寧寺、乾隆二、三十年代間為紀念蒙古民族歸附覲見先後營建的安遠廟、普樂寺、普陀宗乘之廟，以及乾隆四十五年(1780)為迎接西藏六世班禪來華並向乾隆皇帝祝壽所興建的須彌福壽之廟等。清帝國透過宗教信仰，藉以鞏固並加強與信奉藏傳佛教蒙藏民族的關係，從而達到北疆國防長治久安的一種政治策略。</p>`,
+                    show_desc: false
                 }
             ],
             active_person: '',
